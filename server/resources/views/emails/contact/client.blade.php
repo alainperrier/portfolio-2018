bb374f3481fe6b8e341cb9964cbb2623b73eb080
oXg3n <a.perrier721@gmail.com>
2018-04-17 11:58:24 +0200

    email site template fixed

--- server/resources/views/emails/contact/client.blade.php
+++ server/resources/views/emails/contact/client.blade.php
@@ -5,14 +5,13 @@
 </head>
 <body>
 	<p>Bonjour {{ $contact->name }},</p>
-	<br>
-	<p>Merci de m'avoir laissé un message sur <a href="http://laravel-nuxt.test/" target="_blank">mon site</a>, je vous apporterai une réponse dès que possible.</a></p>
+	<p>Merci de m'avoir laissé un message sur <a href="https://alainperrier.me/" target="_blank">mon site</a>, je vous apporterai une réponse dès que possible.</a></p>
 	<p>En attendant, voici un récapitulatif des informations que vous m'avez transmises :</a></p>
 	<p><strong>Nom complet</strong> : {{ $contact->name }}</p>
 	<p><strong>Email</strong> : {{ $contact->email }}</p>
 	<p><strong>Message</strong> : {{ $contact->message }}<p>
 	<br>
-	<p>Cordialement,<p>
+	<p>A très vite,<p>
 	<p>Alain Perrier<p>
 </body>
 </html>
