2dc2b90ed6d93c928fb8ec36fa8b344f044fb088
oXg3n <a.perrier721@gmail.com>
2018-04-18 23:53:26 +0200

    core-js new test

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -132,7 +132,7 @@ module.exports = {
     /*
       ** Add vendor (to better cache resources)
     */
-    vendor: ['core-js/es6/object', 'core-js/es6/promise', 'core-js/es7/promise', 'core-js/es7/object', 'core-js/es7/observable', 'core-js/es7/symbol', 'core-js/es7/global', 'core-js/es7/system', 'core-js/es7/map', 'core-js/es6/symbol', 'core-js/web'],
+    vendor: ['core-js/es7'],
     // vendor: ['core-js/core', 'core-js/es6', 'core-js/es7', 'core-js/web'],
     /*
       ** PostCss Plugins
