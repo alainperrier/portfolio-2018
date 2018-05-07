208044d27c031ffc9f9f53c0d9659a3e08936fc7
alainperrier <a.perrier721@gmail.com>
2018-05-08 00:52:34 +0200

    Animation issue on landing page fixed

--- client/components/Landing.vue
+++ client/components/Landing.vue
@@ -4,10 +4,7 @@
     overflow-y-scroll smp:overflow-hidden sml:overflow-hidden md:overflow-hidden lg:overflow-hidden xl:overflow-hidden xxl:overflow-hidden">
     <div class="flex flex-col smp:flex-row sml:flex-row md:flex-row lg:flex-row xl:flex-row xxl:flex-row items-center w-full smp:w-4/5 sml:w-4/5 md:w-4/5 lg:w-4/5 xl:w-4/5 xxl:w-4/5">
       <div
-        class="flex flex-row items-center justify-center flex-no-shrink logo
-        w-full smp:w-50-15 sml:w-50-15 md:w-50-15 lg:w-50-15 xl:w-50-15 xxl:w-50-15
-        pr-0 smp:pr-12 sml:pr-12 md:pr-12 lg:pr-14 xl:pr-16 xxl:pr-16
-        mb-6 smp:mb-0 sml:mb-0 md:mb-0 lg:mb-0 xl:mb-0 xxl:mb-0">
+        class="flex flex-row items-center justify-center flex-no-shrink logo w-full smp:w-50-15 sml:w-50-15 md:w-50-15 lg:w-50-15 xl:w-50-15 xxl:w-50-15 pr-0 smp:pr-12 sml:pr-12 md:pr-12 lg:pr-14 xl:pr-16 xxl:pr-16 mb-6 smp:mb-0 sml:mb-0 md:mb-0 lg:mb-0 xl:mb-0 xxl:mb-0">
         <div
           class="flex flex-col items-center justify-center flex-no-shrink
           m-0 smp:ml-auto sml:ml-auto md:ml-auto lg:ml-auto xl:ml-auto xxl:ml-auto">
@@ -25,12 +22,7 @@
         </div>
       </div>
       <div
-        class="flex flex-col border-white border-solid flex-no-shrink description
-        w-auto smp:w-1/2 sml:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 xxl:w-1/2
-        border-l-0 smp:border-l-3 sml:border-l-3 md:border-l-3 lg:border-l-3 xl:border-l-3 xxl:border-l-3
-        border-t-2 smp:border-t-0 sml:border-t-0 md:border-t-0 lg:border-t-0 xl:border-t-0 xxl:border-t-0
-        pl-0 smp:pl-8 sml:pl-10 md:pl-10 lg:pl-10 xl:pl-10 xxl:pl-10
-        pt-2 smp:pt-0 sml:pt-0 md:pt-0 lg:pt-0 xl:pt-0 xxl:pt-0">
+        class="flex flex-col border-white border-solid flex-no-shrink description w-auto smp:w-1/2 sml:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 xxl:w-1/2 border-l-0 smp:border-l-3 sml:border-l-3 md:border-l-3 lg:border-l-3 xl:border-l-3 xxl:border-l-3 border-t-2 smp:border-t-0 sml:border-t-0 md:border-t-0 lg:border-t-0 xl:border-t-0 xxl:border-t-0 pl-0 smp:pl-8 sml:pl-10 md:pl-10 lg:pl-10 xl:pl-10 xxl:pl-10 pt-2 smp:pt-0 sml:pt-0 md:pt-0 lg:pt-0 xl:pt-0 xxl:pt-0">
         <div
           class="flex flex-col flex-no-shrink
           text-center smp:text-left sml:text-left md:text-left lg:text-left xl:text-left xxl:text-left
